aed376e7d8703fba45fde0f758d7521a7a96c9c0
Jarno Elovirta <jarno@elovirta.com>
2017-12-06 08:41:39 +0200

    Add CORS

--- app/Filters.scala
+++ app/Filters.scala
@@ -3,8 +3,11 @@ import javax.inject._
 import filters.TokenAuthorizationFilter
 import play.api._
 import play.api.http.DefaultHttpFilters
+import play.filters.cors.CORSFilter
 
 @Singleton
-class Filters @Inject()(env: Environment, tokenAuthorizationFilter: TokenAuthorizationFilter)
-  extends DefaultHttpFilters(tokenAuthorizationFilter) {
+class Filters @Inject()(env: Environment,
+                        tokenAuthorizationFilter: TokenAuthorizationFilter,
+                        corsFilter: CORSFilter)
+  extends DefaultHttpFilters(corsFilter, tokenAuthorizationFilter) {
 }
