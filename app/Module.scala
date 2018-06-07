b8efc1c22902d6f7641914bc1ab76f73ee93810a
Jarno Elovirta <jarno@elovirta.com>
2018-06-07 18:25:44 +0300

    Clean

--- app/Module.scala
+++ app/Module.scala
@@ -6,8 +6,6 @@ import services._
 class Module extends AbstractModule {
 
   override def configure() = {
-//    bind(classOf[Queue]).to(classOf[DBQueue]).asEagerSingleton()
-//    bind(classOf[Dispatcher]).to(classOf[DBQueue])
     bind(classOf[Queue]).to(classOf[SimpleQueue]).asEagerSingleton()
     bind(classOf[Dispatcher]).to(classOf[SimpleQueue])
     bind(classOf[LogStore]).to(classOf[SimpleLogStore])
