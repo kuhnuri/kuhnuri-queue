e9b33968ccd9c3182a3120e50515f504a317481c
Jarno Elovirta <jarno@elovirta.com>
2018-12-05 10:08:46 +0200

    Add simple health check

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,13 +1,21 @@
 package controllers
 
 import javax.inject._
+import play.api.Logger
 import play.api.mvc._
 
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
+  private val logger = Logger(this.getClass)
+
   def index = Action {
     Redirect("index.html")
+  }
+
+  def health = Action {
+    logger.info("Check health")
+    Ok("")
   }
 
 }
